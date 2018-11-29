230e99974e16b8daaed89834fe94adb578261d5b
Logon Aluno <logonrmlocal@fiap.com.br>
2018-11-28 22:29:29 -0200

    eu gostei

---
+++ Aula01/ExemploStorage/ExemploStorage/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ExemploStorage
+{
+    public class Aluno : TableEntity
+    {
+        public Aluno(string ra, string cidade) : base(cidade, ra)
+        {
+
+        }
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ Aula01/ExemploStorage/ExemploStorage/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ExemploStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storage13netigor;AccountKey=gDhuZlVAUkXSs/iNmMWZEnG9T0cC/2pJ6zX3+x3hOsRZ4N4ev0G7FKDhumMhXlEH/nl2xXbqtRlzUbmQWxr5YQ==;EndpointSuffix=core.windows.net");
+
+            #region blob
+            //var blobClient = account.CreateCloudBlobClient();
+            //var container = blobClient.GetContainerReference("rm330381");
+
+            //container.CreateIfNotExistsAsync().Wait();
+
+            //var blob = container.GetBlockBlobReference("igor3.txt");
+            //blob.UploadTextAsync("Olá Mundo !");
+
+            //var sas = blob.GetSharedAccessSignature(new SharedAccessBlobPolicy()
+            //{
+            //    Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write, //Necessário para poder ter acesso a leitura
+            //    SharedAccessExpiryTime = DateTime.Now.AddYears(5) //Necessário passar uma SE (Shared Expiry) para funcionar
+            //});
+
+            //Console.WriteLine(blob.Uri + sas);
+            #endregion
+
+            #region table (na verdade é chave, valor kkk)
+            var client = account.CreateCloudTableClient();
+            var table = client.GetTableReference("rm330381");
+            table.CreateIfNotExistsAsync().Wait();
+
+            var entity = new Aluno("rm330381", "sao paulo");
+            entity.Nome = "Igor";
+            entity.Email = "igorcabralfacul@gmail.com";
+
+            table.ExecuteAsync(TableOperation.Insert(entity));
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
